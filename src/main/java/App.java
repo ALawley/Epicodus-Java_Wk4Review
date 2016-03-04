93427d236e8065ac8e2036892e03061c7e2259b4
Anna Lawley <student@epicodus.com>
2016-03-04 11:18:00 -0800

    add integration tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,130 @@
 import java.util.Map;
 import java.util.HashMap;
+import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
-import static spark.Spark.*;
+import java.util.List;
 
 public class App {
   public static void main(String[] args) {
-    
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/bands", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("bands", Band.all());
+      model.put("template", "templates/bands.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/venues", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("venues", Venue.all());
+      model.put("template", "templates/venues.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/bands", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Band newBand = new Band(name);
+      newBand.save();
+      model.put("bands", Band.all());
+      model.put("template", "templates/bands.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/venues", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Venue newVenue = new Venue(name);
+      newVenue.save();
+      model.put("venues", Venue.all());
+      model.put("template", "templates/venues.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/bands/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Band band = Band.find(Integer.parseInt(request.params(":id")));
+
+      model.put("band", band);
+      model.put("venues", Venue.all());
+      model.put("assignedvenues", band.getVenues());
+      model.put("template", "templates/band.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/venues/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Venue venue = Venue.find(Integer.parseInt(request.params(":id")));
+
+      model.put("venue", venue);
+      model.put("bands", venue.getBands());
+      model.put("template", "templates/venue.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/bands/:id/update", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Band band = Band.find(Integer.parseInt(request.params(":id")));
+      String name = request.queryParams("name");
+      band.update(name);
+      Band updatedBand = Band.find(band.getId());
+      model.put("band", updatedBand);
+      model.put("venues", Venue.all());
+      model.put("assignedvenues", updatedBand.getVenues());
+      model.put("template", "templates/band.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/venues/:id/update", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Venue venue = Venue.find(Integer.parseInt(request.params(":id")));
+      String name = request.queryParams("name");
+      venue.update(name);
+      Venue updatedVenue = Venue.find(venue.getId());
+      model.put("venue", updatedVenue);
+      model.put("bands", updatedVenue.getBands());
+      model.put("template", "templates/venue.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/bands/:id/delete", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Band band = Band.find(Integer.parseInt(request.params(":id")));
+      band.delete();
+      model.put("bands", Band.all());
+      model.put("template", "templates/bands.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/venues/:id/delete", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Venue venue = Venue.find(Integer.parseInt(request.params(":id")));
+      venue.delete();
+      model.put("venues", Venue.all());
+      model.put("template", "templates/venues.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/bands/:bandid/addvenue/:venueid", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int bandId = Integer.parseInt(request.params(":bandid"));
+      int venueId = Integer.parseInt(request.params(":venueid"));
+      Band band = Band.find(bandId);
+      band.addVenue(venueId);
+      model.put("band", band);
+      model.put("venues", Venue.all());
+      model.put("assignedvenues", band.getVenues());
+      model.put("template", "templates/band.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
-
-
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -19,4 +19,81 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("I want to view");
+  }
+
+  @Test
+  public void bandAddTest() {
+    goTo("http://localhost:4567/bands");
+    fill("#name").with("Agalloch");
+    submit("#addband");
+    assertThat(pageSource()).contains("Agalloch");
+  }
+
+  @Test
+  public void venueAddTest() {
+    goTo("http://localhost:4567/venues");
+    fill("#name").with("Hawthorne Theater");
+    submit("#addvenue");
+    assertThat(pageSource()).contains("Hawthorne Theater");
+  }
+
+  @Test
+  public void bandUpdateTest() {
+    Band testBand = new Band("Agalloch");
+    testBand.save();
+    String bandpage = String.format("http://localhost:4567/bands/%d", testBand.getId());
+    goTo(bandpage);
+    fill("#name").with("Primordial");
+    submit("#updateband");
+    assertThat(pageSource()).contains("Primordial");
+  }
+
+  @Test
+  public void venueUpdateTest() {
+    Venue testVenue = new Venue("Hawthorne Theater");
+    testVenue.save();
+    String venuepage = String.format("http://localhost:4567/venues/%d", testVenue.getId());
+    goTo(venuepage);
+    fill("#name").with("The Casbah");
+    submit("#updatevenue");
+    assertThat(pageSource()).contains("The Casbah");
+  }
+
+  @Test
+  public void bandDeleteTest() {
+    Band testBand = new Band("Agalloch");
+    testBand.save();
+    String bandpage = String.format("http://localhost:4567/bands/%d", testBand.getId());
+    goTo(bandpage);
+    submit("#deleteband");
+    assertThat(pageSource()).doesNotContain("Agalloch");
+  }
+
+  @Test
+  public void venueDeleteTest() {
+    Venue testVenue = new Venue("Hawthorne Theater");
+    testVenue.save();
+    String venuepage = String.format("http://localhost:4567/venues/%d", testVenue.getId());
+    goTo(venuepage);
+    submit("#deletevenue");
+    assertThat(pageSource()).doesNotContain("Hawthorne Theater");
+  }
+
+  @Test
+  public void venueAssignTest() {
+    Band testBand = new Band("Agalloch");
+    testBand.save();
+    Venue testVenue = new Venue("Hawthorne Theater");
+    testVenue.save();
+    String bandpage = String.format("http://localhost:4567/bands/%d", testBand.getId());
+    goTo(bandpage);
+    String venuebutton = String.format("#addvenue%d", testVenue.getId());
+    submit(venuebutton);
+    String venuelink = String.format("/venues/%d", testVenue.getId());
+    assertThat(pageSource()).contains(venuelink);
+  }
 }
