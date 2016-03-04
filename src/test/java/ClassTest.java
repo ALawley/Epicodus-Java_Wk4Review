cd0026f6e14739ec7b8d54b874a08510e833a5a7
Anna Lawley <student@epicodus.com>
2016-03-04 08:30:38 -0800

    initial commit

---
+++ src/test/java/ClassTest.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ClassTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void method_input_result() {
+    // eventually put your testing code here
+  }
+}
---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,21 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/concert_tracker_test", null, null);
+
+   }
+
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteBandQuery = "DELETE FROM bands *;";
+      String deleteVenueQuery = "DELETE FROM venues *;";
+      String deleteConcertQuery = "DELETE FROM concerts *;";
+      con.createQuery(deleteBandQuery).executeUpdate();
+      con.createQuery(deleteVenueQuery).executeUpdate();
+      con.createQuery(deleteConcertQuery).executeUpdate();
+    }
+  }
+}
